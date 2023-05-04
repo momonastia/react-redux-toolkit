2304517dd808a0859e5900844e3c24b82c12b34d
momonastia~ <anastasiatyrikova@gmail.com>
2023-05-04 10:37:57 +0200

    Feat: main stuctire of todo with adding logic added

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString,
+          text,
+          complited: false,
+        },
+      ]);
+      setText("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={addTodo}>Add todo</button>
+      </label>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <span className="delete">&times;</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
